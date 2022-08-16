f60857b6edb0aa9daa79fcc8d07bf92108aed976
GBachheimer <gbachheimer@icloud.com>
2022-08-16 09:52:04 +0300

    copy text button added

--- src/components/card.js
+++ src/components/card.js
@@ -5,7 +5,7 @@ export default function Card(props) {
         <div className = {`text-center card text-bg-${colors[randomColor]} my-2 position-relative start-50 translate-middle-x`} style = {{ width: "80%", maxWidth: "80%"}}>
             <div className = "card-header">{props.title}</div>
             <div className = "card-body">
-                <p className = "card-text">{props.text}</p>
+                <p className = "card-text" id = "cardText">{props.text}</p>
             </div>
         </div>
     );
--- src/routes/UniquePage.jsx
+++ src/routes/UniquePage.jsx
@@ -25,18 +25,31 @@ export default function UniquePage(props) {
         const url = window.location.href;
         navigator.clipboard.writeText(url);
     }
+    function copyText() {
+        navigator.clipboard.writeText(dataObtained.pasteText);
+    }
     return (
         <div >
             <NavBar />
             <Card title = {dataObtained.pasteTitle} text = {dataObtained.pasteText}/>
-            <button 
-                className = "position-relative start-50 translate-middle-x btn btn-outline-light rounded-pill" 
-                style = {{ width: "40%", maxWidth: "40%"}} 
-                type = "button"
-                onClick = {copyURL}
-            >
-                Copy Link
-            </button>
+            <div className = "text-center">
+                <button 
+                    className = "btn btn-outline-light rounded-pill mx-1" 
+                    style = {{ width: "20%", maxWidth: "20%"}} 
+                    type = "button"
+                    onClick = {copyURL}
+                >
+                    Copy Link
+                </button>
+                <button 
+                    className = "btn btn-outline-light rounded-pill mx-1" 
+                    style = {{ width: "20%", maxWidth: "20%"}} 
+                    type = "button"
+                    onClick = {copyText}
+                >
+                    Copy Text
+                </button>
+            </div>
             <p className = "fixed-bottom text-center text-white">
                 You are currently not logged in, this means you can not edit or delete anything you paste.&nbsp;
                 <Link to = "/signUp" className = "text-white">Sign Up</Link> or&nbsp;
